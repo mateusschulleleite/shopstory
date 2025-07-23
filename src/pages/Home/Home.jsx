979957c22a5015c5c0766c6de3dd60fe3185ae5b
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2025-07-23 00:04:35 -0300

    Botao de lixeira adicionado na listagem de páginas

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import "./Home.scss";
 import Nav from "../../components/Nav/Nav";
 import Header from "../../components/Header/Header";
-import { FaPager } from "react-icons/fa";
 import { LuPencil } from "react-icons/lu";
 import { Link } from "react-router";
+import { RiDeleteBin6Line, RiPagesLine } from "react-icons/ri";
 
-export default function Home({urls}) {
+export default function Home({ urls }) {
   return (
     <div className="home d-flex bg-light">
       <Nav />
@@ -22,15 +22,23 @@ export default function Home({urls}) {
                 key={key}
                 className="d-flex justify-content-between bg-white p-3 rounded-3"
               >
-                <div className="d-flex align-items-center gap-3">
+                <div className="d-flex align-items-center gap-2">
                   <div className="d-flex fs-5 align-items-center">
-                    <FaPager />
+                    <RiPagesLine />
                   </div>
                   <span>{item.urlPage}</span>
                 </div>
-                <Link to={`/editpage/${key}`} className="d-flex fs-5 align-items-center text-danger">
-                  <LuPencil />
-                </Link>
+                <div className="d-flex align-items-center gap-2">
+                  <Link
+                    to={`/editpage/${key}`}
+                    className="d-flex fs-5 align-items-center text-danger"
+                  >
+                    <LuPencil />
+                  </Link>
+                  <div className="d-flex fs-5 align-items-center text-danger">
+                    <RiDeleteBin6Line />
+                  </div>
+                </div>
               </li>
             ))}
           </ul>
